7432f8f00b8c6c9143430c827392385ddb6111af
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-08-19 21:54:44 +0530

    Configure mongodb and cloudinary storage

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // app config
 const app = express();
 const port = process.env.PORT ;
+connectDB();
+connectCloudinary();
 
 // middlewares
 app.use(express.json());
